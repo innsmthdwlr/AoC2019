102347155d91a185f6baddf2e2071dbc1e6f59af
innsmouth_dweller <innsmouth.dweller@gmail.com>
2019-12-02 14:30:14 +0100

    cleaned up a bit

--- Day01/main.py
+++ Day01/main.py
@@ -1,19 +1,20 @@
 import math
 import os
+from functools import reduce
 
 with open(f'{os.path.dirname(os.path.realpath(__file__))}/input.txt', 'r') as reader:
-    mass = [int(x) for x in reader.read().splitlines()]
+    mass = [int(value) for value in reader.read().splitlines()]
 
 def calculate_fuel(mass):
-  fuel_required = math.floor(mass / 3) - 2
+    return math.floor(mass / 3) - 2
+
+def calculate_fuel_total(mass):
+  fuel_required = calculate_fuel(mass)
   if fuel_required <= 0:
     return 0
   else:
-    return fuel_required + calculate_fuel(fuel_required)
+    return fuel_required + calculate_fuel_total(fuel_required)
 
 if __name__ == '__main__':
-    fuel_req = sum([math.floor(x / 3) - 2 for x in mass])
-    total_fuel_req = sum([calculate_fuel(x) for x in mass])
-
-    print(fuel_req)
-    print(total_fuel_req)
+    print(sum([calculate_fuel(x) for x in mass]))
+    print(sum([calculate_fuel_total(x) for x in mass]))
--- Day02/main.py
+++ Day02/main.py
@@ -1,6 +1,6 @@
 import os
     
-def reset_values(program_input, noun, verb):
+def reset_input(program_input, noun, verb):
   reset = program_input[:]
   reset[1] = noun
   reset[2] = verb
@@ -14,31 +14,29 @@ def run_program(program_input):
     pos_a = program_input[next_opcode+1]
     pos_b = program_input[next_opcode+2]
     pos_result = program_input[next_opcode+3]
-    result = 0
 
     if opcode == 1:
-      result = program_input[pos_a] + program_input[pos_b]
+      program_input[pos_result] = program_input[pos_a] + program_input[pos_b]
     elif opcode == 2:
-      result = program_input[pos_a] * program_input[pos_b]
+      program_input[pos_result] = program_input[pos_a] * program_input[pos_b]
 
-    program_input[pos_result] = result
     next_opcode += 4
     opcode = program_input[next_opcode]
   return program_input[0]
 
-def part2(program_input, required_output):
+def find_noun_and_verb(program_input, required_output):
   max_pos = len(program_input)-1
   for noun in range(0, max_pos):
     for verb in range(0, max_pos):
-      comm = reset_values(program_input, noun, verb)
-      result = run_program(comm)
+      command_list = reset_input(program_input, noun, verb)
+      result = run_program(command_list)
       if result == required_output:
-        return 100 * comm[1] + comm[2]
+        return 100 * command_list[1] + command_list[2]
 
 
 if __name__ == '__main__':
     with open(f'{os.path.dirname(os.path.realpath(__file__))}/input.txt', 'r') as reader:
-        puzzle_input = [int(x) for x in reader.read().split(',')]
+        puzzle_input = [int(value) for value in reader.read().split(',')]
 
-    print(run_program(reset_values(puzzle_input,12,2)))
-    print(part2(puzzle_input, 19690720))
+    print(run_program(reset_input(puzzle_input, 12, 2)))
+    print(find_noun_and_verb(puzzle_input, 19690720))
