efc838cdf5d8cf0dad7e3c6ab734c036261ee367
innsmouth_dweller <innsmouth.dweller@gmail.com>
2019-12-02 11:45:07 +0100

    input moved from script to respective txt files

--- Day01/main.py
+++ Day01/main.py
@@ -1,105 +1,8 @@
 import math
+import os
 
-mass = [139301, 
-84565, 
-124180, 
-133902, 
-138726, 
-62665, 
-142967, 
-95598, 
-118044, 
-73234, 
-76476, 
-51634, 
-71582, 
-63619, 
-148430, 
-134733, 
-101537, 
-101140, 
-144543, 
-102233, 
-62048, 
-128633, 
-130113, 
-92531, 
-73820, 
-54964, 
-103485, 
-96364, 
-104119, 
-121954, 
-79215, 
-99235, 
-120179, 
-69237, 
-145584, 
-79193, 
-50684, 
-146481, 
-67783, 
-112741, 
-85024, 
-62298, 
-54083, 
-137704, 
-116561, 
-76862, 
-81410, 
-96341, 
-89992, 
-132926, 
-97955, 
-74751, 
-147553, 
-121496, 
-113303, 
-119671, 
-120871, 
-114278, 
-125628, 
-144275, 
-78826, 
-87092, 
-65883, 
-87517, 
-93974, 
-55358, 
-100922, 
-113304, 
-115728, 
-144556, 
-91728, 
-86367, 
-55283, 
-101841, 
-55454, 
-140703, 
-70706, 
-98173, 
-106920, 
-126984, 
-148960, 
-77909, 
-128304, 
-140036, 
-81044, 
-141419, 
-126770, 
-52787, 
-115783, 
-128647, 
-125986, 
-124506, 
-113935, 
-142203, 
-106404, 
-78433, 
-146573, 
-68575, 
-63563, 
-115616]
+with open(f'{os.path.dirname(os.path.realpath(__file__))}/input.txt', 'r') as reader:
+    mass = [int(x) for x in reader.read().splitlines()]
 
 def calculate_fuel(mass):
   fuel_required = math.floor(mass / 3) - 2
@@ -108,8 +11,9 @@ def calculate_fuel(mass):
   else:
     return fuel_required + calculate_fuel(fuel_required)
 
-fuel_req = sum([math.floor(x / 3) - 2 for x in mass])
-total_fuel_req = sum([calculate_fuel(x) for x in mass])
+if __name__ == '__main__':
+    fuel_req = sum([math.floor(x / 3) - 2 for x in mass])
+    total_fuel_req = sum([calculate_fuel(x) for x in mass])
 
-print(fuel_req)
-print(total_fuel_req)
+    print(fuel_req)
+    print(total_fuel_req)
