58c5d313a3f2144bda1a35ddf28818e61ace8c89
innsmouth_dweller <innsmouth.dweller@gmail.com>
2019-12-04 08:43:04 +0100

    remove duplicate checks -_-

--- Day04/main.py
+++ Day04/main.py
@@ -8,13 +8,13 @@ def never_decrease(password_candidate):
 
 def adjacent_are_the_same(password_candidate):
     password_candidate_str = str(password_candidate)
-    if len(set(password_candidate_str)) <= 5 and password_candidate_str == ''.join(sorted(password_candidate_str)):
+    if len(set(password_candidate_str)) <= 5:
         return True
     return False
 
 def adjacent_are_the_same_p2(password_candidate):
     password_candidate_str = str(password_candidate)
-    if 2 in Counter(password_candidate_str).values() and password_candidate_str == ''.join(sorted(password_candidate_str)):
+    if 2 in Counter(password_candidate_str).values():
         return True
     return False
 
