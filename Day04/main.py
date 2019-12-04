229c0c0d43300e3ff6a3e379256bd4d7d8102e4b
innsmouth_dweller <innsmouth.dweller@gmail.com>
2019-12-04 08:44:41 +0100

    clean up

--- Day04/main.py
+++ Day04/main.py
@@ -2,21 +2,15 @@ from collections import Counter
 
 def never_decrease(password_candidate):
     password_candidate_str = str(password_candidate)
-    if ''.join(sorted(password_candidate_str)) == password_candidate_str:
-        return True
-    return False
+    return ''.join(sorted(password_candidate_str)) == password_candidate_str
 
 def adjacent_are_the_same(password_candidate):
     password_candidate_str = str(password_candidate)
-    if len(set(password_candidate_str)) <= 5:
-        return True
-    return False
+    return len(set(password_candidate_str)) <= 5
 
 def adjacent_are_the_same_p2(password_candidate):
     password_candidate_str = str(password_candidate)
-    if 2 in Counter(password_candidate_str).values():
-        return True
-    return False
+    return 2 in Counter(password_candidate_str).values()
 
 if __name__ == '__main__':
     input_range = range(359282, 820402)
